f338db16175334b3bd39317cbca74522baafc6e6
Georg Wille <georg.wille@mailbox.org>
2022-12-02 19:11:35 +0100

    another byte

--- aoc_02ab_golf.py
+++ aoc_02ab_golf.py
@@ -1,1 +1,1 @@
-print([sum(int('417852396312456897'[e+o])for e in[ord(l[0])-329+3*ord(l[2])for l in open('i')])for o in [0,9]])
+print([sum(int('417852396312456897'[e+o])for e in[ord(l[0])-329+3*ord(l[2])for l in open('i')])for o in[0,9]])
