b16f4c306baa4aedba85a0a36904f4c8b53c0a6d
Georg Wille <georg.wille@mailbox.org>
2022-12-02 17:04:58 +0100

    shaved a few bytes still

--- aoc_02ab_golf.py
+++ aoc_02ab_golf.py
@@ -1,3 +1,1 @@
-s='417852396312456897'
-p=[ord(l[0])+3*ord(l[2])-329 for l in open('i')]
-print([sum(int(s[e+o])for e in p)for o in [0,9]])
+print([sum(int('417852396312456897'[e+o])for e in [ord(l[0])+3*ord(l[2])-329 for l in open('i')])for o in [0,9]])
