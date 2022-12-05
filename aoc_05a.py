239e93c23c22986ad599e204767485ba200bae46
Georg Wille <georg.wille@mailbox.org>
2022-12-05 13:25:40 +0100

    added day 05

---
+++ aoc_05a.py
@@ -0,0 +1,21 @@
+f = open("input_05.txt")
+
+field = [[], [], [], [], [], [], [], [], [], []]
+
+for line in f:
+    line = line.strip("\n")
+    if not (line):
+        break
+    for i in range(1, 10):
+        c = line[4 * i - 3]
+        if c.isalpha():
+            field[i].insert(0, c)
+
+for line in f:
+    _, cnt, _, src, _, tar = line.split(" ")
+    cnt, src, tar = int(cnt), int(src), int(tar)
+    field[tar] += field[src][:-cnt-1:-1]
+    field[src] = field[src][:-cnt]
+
+for stack in field[1:]:
+    print(stack[-1], end="")
